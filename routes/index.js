9a16dda4ff261edd05624c7260ed58287b561d70
huuhuybnPC <huuhuybn@gmail.com>
2021-12-04 08:35:04 +0700

    update best site

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', express.static('template/public'));
+router.post('/downloader', function (req, res) {
+
+});
 
 module.exports = router;
